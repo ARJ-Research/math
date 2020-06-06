333cb04967b47a197006b772c3548863676d1a5d
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-06-06 18:56:57 +0800

    Copy adjoints instead of accumulate

--- stan/math/rev/functor/map_variadic.hpp
+++ stan/math/rev/functor/map_variadic.hpp
@@ -5,6 +5,7 @@
 #include <stan/math/prim/functor.hpp>
 #include <stan/math/prim/functor/map_variadic.hpp>
 #include <stan/math/rev/core.hpp>
+#include <stan/math/rev/core/save_adjoints.hpp>
 #include <tbb/task_arena.h>
 #include <tbb/parallel_reduce.h>
 #include <tbb/blocked_range.h>
@@ -24,17 +25,15 @@ struct map_variadic_impl<ReduceFunction, ReturnType,
   struct recursive_applier {
     double* partials_;
     double* vals__;
-    double* adjs__;
     ReturnType result_;
     std::ostream* msgs_;
     std::tuple<Args...> args_;
 
     recursive_applier(double* sliced_partials, double* vals_,
-                      double* adjs_, ReturnType&& result,
+                      ReturnType&& result,
                       std::ostream* msgs, Args&&... args)
         : partials_(sliced_partials),
           vals__(vals_),
-          adjs__(adjs_),
           result_(std::forward<ReturnType>(result)),
           msgs_(msgs),
           args_(std::forward<Args>(args)...) {}
@@ -64,26 +63,22 @@ struct map_variadic_impl<ReduceFunction, ReturnType,
 
               // Copy calculated value
               vals__[i] = sub_v.val();
-
-              // Copy calculated value
-              adjs__[i] = sub_v.adj();
             }
           },
           args_tuple_local_copy);
-
-      // Accumulate adjoints of all arguments
-      apply(
-          [&](auto&&... args) {
-            accumulate_adjoints(partials_,
-                                std::forward<decltype(args)>(args)...);
-          },
-          std::move(args_tuple_local_copy));
+	      // Copy adjoints of arguments
+	      apply(
+	          [&](auto&&... args) {
+	            save_adjoints(partials_,
+	                          std::forward<decltype(args)>(args)...);
+	          },
+	          std::move(args_tuple_local_copy));
     }
   };
 
-  inline std::decay_t<ReturnType> operator()(ReturnType&& result,
-                               int grainsize, std::ostream* msgs,
-                               Args&&... args) const {
+  inline ReturnType operator()(ReturnType&& result, int grainsize,
+  							   std::ostream* msgs, Args&&... args) const {
+
     const std::size_t num_iter = result.size();
     const std::size_t num_vars_all_terms = count_vars(args...);
 
@@ -91,27 +86,23 @@ struct map_variadic_impl<ReduceFunction, ReturnType,
         num_vars_all_terms);
     double* partials = ChainableStack::instance_->memalloc_.alloc_array<double>(
         num_vars_all_terms);
-
-    Eigen::Map<vector_d>(partials, num_vars_all_terms).setZero();
+    Eigen::VectorXd vals_(num_iter);
 
     save_varis(varis, args...);
-    Eigen::VectorXd vals_(num_iter);
-    Eigen::VectorXd adjs_(num_iter);
 
-    recursive_applier worker(partials, &vals_[0], &adjs_[0],
+    recursive_applier worker(partials, &vals_[0],
                              std::forward<ReturnType>(result), msgs,
                              std::forward<Args>(args)...);
 
-
     tbb::parallel_for(
         tbb::blocked_range<std::size_t>(0, num_iter, grainsize), worker);
-
+/*
     for(size_t i = 0; i < num_iter; ++i) {
       result[i] = var(new precomputed_gradients_vari(
                             vals_[i], num_vars_all_terms, varis,
                             partials));
     }
-
+*/
     return result;
   }
 };
