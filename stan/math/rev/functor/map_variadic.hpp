2cb4e4f088a322a566c8cb8e991812377c30ab8f
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-06-24 20:38:44 +0800

    Copy arguments

--- stan/math/rev/functor/map_variadic.hpp
+++ stan/math/rev/functor/map_variadic.hpp
@@ -1,12 +1,19 @@
 #ifndef STAN_MATH_REV_FUNCTOR_MAP_VARIADIC_HPP
 #define STAN_MATH_REV_FUNCTOR_MAP_VARIADIC_HPP
 
+#include <stan/math/prim/meta.hpp>
+#include <stan/math/prim/functor.hpp>
 #include <stan/math/prim/functor/map_variadic.hpp>
 #include <stan/math/rev/core.hpp>
-#include <stan/math/rev/core/precomputed_gradients.hpp>
 #include <stan/math/rev/core/nested_rev_autodiff.hpp>
+#include <stan/math/rev/core/precomputed_gradients.hpp>
 #include <stan/math/rev/core/save_adjoints.hpp>
-#include <iostream>
+#include <tbb/task_arena.h>
+#include <tbb/parallel_reduce.h>
+#include <tbb/blocked_range.h>
+
+#include <tuple>
+#include <vector>
 
 namespace stan {
 namespace math {
@@ -17,43 +24,88 @@ template <typename ApplyFunction, typename ReturnType, typename... Args>
 struct map_variadic_impl<ApplyFunction, ReturnType,
                          require_st_var<ReturnType>, Args...> {
 
-  template <typename TupleT>
   struct recursive_applier {
-    var* result_;
+    double* partials_;
+    double* vals_;
+    ReturnType result_;
     std::ostream* msgs_;
-    TupleT* args_;
+    std::tuple<Args...> args_;
 
-    recursive_applier(var* result,
-                      std::ostream* msgs, TupleT* tuple_args)
-        : result_(result),
+    recursive_applier(double* sliced_partials, double* values,
+                      ReturnType&& result,
+                      std::ostream* msgs, Args&&... args)
+        : partials_(sliced_partials),
+          vals_(values),
+          result_(std::forward<ReturnType>(result)),
           msgs_(msgs),
-          args_(tuple_args) {}
+          args_(std::forward<Args>(args)...) {}
 
     inline void operator()(const tbb::blocked_range<size_t>& r) const {
-      apply([&](auto&&... args) {
-            for (size_t i = r.begin(); i < r.end(); ++i) {
-              result_[i] = ApplyFunction()(i, args...);
-              std::cout << i << std::endl << result_[i].val() << std::endl
-                        << result_[i].adj() << std::endl;
-            }
+      // Initialize nested autodiff stack
+      const nested_rev_autodiff begin_nest;
+
+      // Create nested autodiff copies of all arguments that do not point
+      //   back to main autodiff stack
+      auto args_tuple_local_copy = apply(
+          [&](auto&&... args) {
+            return std::tuple<decltype(deep_copy_vars(args))...>(
+                deep_copy_vars(args)...);
           },
           args_);
+
+      apply(
+          [&](auto&&... args) {
+            for (size_t i = r.begin(); i < r.end(); ++i) {
+
+              // Perform calculation
+              var sub_v = ApplyFunction()(i, args...);
+
+              // Compute Jacobian
+              sub_v.grad();
+
+              // Copy calculated value
+              vals_[i] = sub_v.val();
+            }
+          },
+          args_tuple_local_copy);
+          // Copy adjoints of arguments
+          apply(
+              [&](auto&&... args) {
+                save_adjoints(partials_,
+                              std::forward<decltype(args)>(args)...);
+              },
+              std::move(args_tuple_local_copy));
     }
   };
 
-  inline auto operator()(ReturnType&& result, int grainsize,
+  inline ReturnType operator()(ReturnType&& result, int grainsize,
                                std::ostream* msgs, Args&&... args) const {
 
     const std::size_t num_iter = result.size();
     const std::size_t num_vars_all_terms = count_vars(args...);
 
-    auto args_tuple = std::make_tuple(args...);
+    vari** varis = ChainableStack::instance_->memalloc_.alloc_array<vari*>(
+        num_vars_all_terms);
+    double* partials = ChainableStack::instance_->memalloc_.alloc_array<double>(
+        num_vars_all_terms);
+    double* values = ChainableStack::instance_->memalloc_.alloc_array<double>(
+        num_iter);
 
-    recursive_applier<decltype(args_tuple)> 
-        worker(result.data(), msgs, (&args_tuple));
+    // Copy varis for all terms
+    save_varis(varis, args...);
+
+    recursive_applier worker(partials, values,
+                             std::forward<ReturnType>(result), msgs,
+                             std::forward<Args>(args)...);
 
     tbb::parallel_for(
         tbb::blocked_range<std::size_t>(0, num_iter, grainsize), worker);
+
+    for(size_t i = 0; i < num_iter; ++i) {
+      result.coeffRef(i) = var(new precomputed_gradients_vari(
+                            values[i], num_vars_all_terms, varis,
+                            partials));
+    }
 
     return result;
   }
