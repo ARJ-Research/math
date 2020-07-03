0ac5c33d4c526ade25a149667cde0a85ca543588
Andrew Johnson <andrew.r.johnson@postgrad.curtin.edu.au>
2020-07-03 15:43:36 +0800

    Revert apply

--- stan/math/prim/functor/apply.hpp
+++ stan/math/prim/functor/apply.hpp
@@ -25,12 +25,6 @@ constexpr decltype(auto) apply_impl(F&& f, Tuple&& t,
                                     std::index_sequence<I...> i) {
   return f(std::forward<decltype(std::get<I>(t))>(std::get<I>(t))...);
 }
-
-template <class F, class Tuple, std::size_t... I>
-constexpr decltype(auto) apply_impl(F&& f, Tuple* t,
-                                    std::index_sequence<I...> i) {
-  return f(std::forward<decltype(std::get<I>(*t))>(std::get<I>(*t))...);
-}
 }  // namespace internal
 /*
  * Call the functor f with the tuple of arguments t, like:
@@ -50,14 +44,6 @@ constexpr decltype(auto) apply(F&& f, Tuple&& t) {
       std::forward<F>(f), std::forward<Tuple>(t),
       std::make_index_sequence<
           std::tuple_size<std::remove_reference_t<Tuple>>{}>{});
-}
-
-template <class F, class Tuple>
-constexpr decltype(auto) apply(F&& f, Tuple* t) {
-  return internal::apply_impl(
-      std::forward<F>(f), t,
-      std::make_index_sequence<
-          std::tuple_size<std::remove_pointer_t<Tuple>>{}>{});
 }
 
 }  // namespace math
